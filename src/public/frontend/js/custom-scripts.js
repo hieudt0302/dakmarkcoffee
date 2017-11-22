ba314c045959e3682088c77c09ceded57457638d
Tôi Là Tùng <toilatung@icloud.com>
2017-11-22 21:49:53 +0700

    Add resources

---
+++ src/public/frontend/js/custom-scripts.js
@@ -0,0 +1,97 @@
+(function($) {
+    "use strict";
+
+    jQuery("#menu-secondary-menu li.menu-item").click(function(e) {
+		e.preventDefault();
+		if ($(this).hasClass("hover")){
+			$(this).removeClass("hover");
+			$(this).children('ul.sub-menu').css({"display": "none"});
+		}else{
+			$(this).addClass("hover");
+			$(this).children('ul.sub-menu').css({"display": "block"});
+		}
+	});
+
+
+	jQuery($("#menu").css("display")).change(function() {
+	    if ($( window ).width() < 768){
+	    	var isMenu = $("#menu").css("display")=='block';
+	    	console.log("IsMenu: "+isMenu);
+	    	if (isMenu){
+	    		$("#menu-secondary-menu").css({"display": "none"});
+	    	}
+	    	else{
+	    		$("#menu-secondary-menu").css({"display": "block"});
+	    	}
+	    }
+	});
+
+	/* RATING BOX */
+    jQuery('#respond a.star').click(function(event) {
+        event.preventDefault();
+        var curID = $(this).attr("id");
+        var yourRt = curID.charAt(0);
+        let el = document.querySelector('#respond'); 
+        var danhgia = el.querySelectorAll('a.star');
+
+        for (var i = 0; i < danhgia.length; i++) {
+            var exiKey = danhgia[i].querySelectorAll('i.danhgia');
+            
+            if (danhgia[i].id != curID){
+                for (var j=0; j < exiKey.length; j++) {
+                    exiKey[j].setAttribute("class", "danhgia icon-star-empty");
+                }
+            }else{
+                for (var j=0; j < exiKey.length; j++) {
+                    exiKey[j].setAttribute("class", "danhgia icon-star");
+                }
+            }
+        }
+        // GET and SET Rating select value
+        $("#yourrating option[value="+yourRt+"]").prop('selected', true);
+        // $('#yourrating').val(yourRt).prop('selected', true);
+    });
+
+
+    $('body').scrollspy({ target: '#product-tabs' })
+    $('#product-tabs').affix({
+      offset: {
+        top: getOffset(1),
+        bottom: getOffset(0)
+      }
+    });
+    $('#product-tabs').on('activate.bs.scrollspy', function () {
+        var children = $("#product-tabs li.active > a").attr("href");
+    });
+
+    function getOffset(isTop){
+        if (isTop == 1){
+            return ($('#Top_bar').outerHeight(true) + $('.product_image_wrapper').outerHeight(true) - 50);
+        }
+        if (isTop == 0){
+            var mainNav = $('.main-nav').outerHeight(true);
+            var mota = $('#mota').outerHeight(true);
+            var specs  = $('#specs').outerHeight(true);
+            var reviewbox  = $('#reviewbox').outerHeight(true);
+            var relate  = $('#related_products').outerHeight(true);
+            return (mainNav+mota+specs+reviewbox+relate+100);
+        }
+
+    }
+
+	// $( ".responsive-menu-toggle" ).click(function() {
+		
+	//     if ($( window ).width() < 768){
+	//     	var isMenu = $("#menu").css("display")=='block';
+	//     	var isActive = $(this).hasClass("active");
+	//     	console.log("IsMenu: "+isMenu);
+	//     	console.log("isActive: "+isMenu);
+	// 		if ( isActive || isMenu ){
+	// 			$("#menu-secondary-menu").css({"display": "none"});
+	// 		}else{
+	// 			$("#menu-secondary-menu").css({"display": "block"});
+	// 		}
+	// 	}
+	// });
+	
+})(jQuery);
--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -25,16 +25,12 @@
 	<!-- <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900" > -->
 	<!-- <link  rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:400,700,900"> -->
 	<!-- -->
-	<link  rel="stylesheet" type="text/css" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-	<link  rel="stylesheet" type="text/css"  href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	{{--<link  rel="stylesheet" type="text/css" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">--}}
+	{{--<link  rel="stylesheet" type="text/css"  href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">--}}
 	<!-- Mobile specific meta -->
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 	<meta name="format-detection" content="telephone-no">
 	<!-- CSS files -->
-	<!-- CSS files -->
-    <link rel="stylesheet" href="{{asset('frontend/css/plugins.css')}}">
-	<link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
-	<link rel="stylesheet" href="{{asset('css/video.css')}}">	
 	<!-- <link rel="stylesheet" href="{{asset('frontend/css/animate.css')}}">
 	<link rel="stylesheet" href="{{asset('frontend/css/bootstrap-dropdownhover.css')}}">
 	<link rel="stylesheet" href="{{asset('frontend/css/font-awesome.min.css')}}">
@@ -50,20 +46,25 @@
 	<link rel="stylesheet" href="{{asset('frontend/css/pushy.css')}}">
 	<link rel="stylesheet" href="{{asset('frontend/css/revicons.css')}}">
 	<link rel="stylesheet" href="{{asset('frontend/css/settings.css')}}"> -->
-	<script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+	<link rel='stylesheet' href='{{asset('frontend/css/global.css')}}'>
+	<link rel='stylesheet' href='{{asset('frontend/css/structure.css')}}'>
+	<link rel='stylesheet' id='style-static' href='{{asset('frontend/css/store.css')}}'>
+	<link rel='stylesheet' id='style-static' href='{{asset('frontend/css/skins/coffee/stylec721.css')}}'>
+	<link rel='stylesheet' href='{{asset('frontend/css/custom.css')}}'>
+	<link rel="stylesheet" href="{{asset('frontend/plugins/rs-plugin/css/settings.css')}}">
 	@yield('header')
 <meta name="google-site-verification" content="SBNKsjbUQHBBai8E7O5TxOH4R1pHOiD1F2qaSZbPxBk" />	
 </head>
-<body>
+<body class="page-parent template-slider color-custom layout-full-width header-stack header-left subheader-transparent sticky-header sticky-white subheader-title-left">
 	 
 	<!-- Mobile Menu -->
-	<div class="mobile-menu">
-    	@include('layouts.mobile_menu')
-    </div>
+	{{--<div class="mobile-menu">--}}
+    	{{--@include('layouts.mobile_menu')--}}
+    {{--</div>--}}
     <!-- End Mobile Menu -->
 
     <!-- Main Page -->
-	<div id="page2" class="allpage">
+	<div id="Wrapper">
 		@yield('top')
 
 		<!-- Navigation panel -->
@@ -73,27 +74,128 @@
 		@yield('content')
 
 		<!-- Foter -->
-		@include('layouts.footer')
+		{{--@include('layouts.footer')--}}
 		<!-- End Foter -->
 		<!-- End Main Page -->
 
  	</div>
-	<script src="{{asset('frontend/js/plugins.js')}}"></script>
-	<script src="{{asset('frontend/js/custom.js')}}"></script>
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src="https://npmcdn.com/isotope-layout@3.0/dist/isotope.pkgd.min.js"></script>
+	<!-- JS -->
+	<script src="{{asset('frontend/js/jquery-2.1.4.min.js')}}"></script>
+	<script src="{{asset('frontend/js/mfn.menu.js')}}"></script>
+	<script src="{{asset('frontend/js/jquery.plugins.js')}}"></script>
+	<script src="{{asset('frontend/js/jquery.jplayer.min.js')}}"></script>
+	<script src="{{asset('frontend/js/animations/animations.js')}}"></script>
+	<script src="{{asset('frontend/js/email.js')}}"></script>
+	<script src="{{asset('frontend/js/scripts.js')}}"></script>
+	<script src="{{asset('frontend/js/bootstrapscrollspy.min.js')}}"></script>
+	<script src="{{asset('frontend/js/custom-scripts.js')}}"></script>
+	<script src="{{asset('frontend/plugins/rs-plugin/js/jquery.themepunch.tools.min.js')}}"></script>
+	<script src="{{asset('frontend/plugins/rs-plugin/js/jquery.themepunch.revolution.min.js')}}"></script>
+	<script src="{{asset('frontend/plugins/rs-plugin/js/extensions/revolution.extension.video.min.js')}}"></script>
+	<script src="{{asset('frontend/plugins/rs-plugin/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
+	<script src="{{asset('frontend/plugins/rs-plugin/js/extensions/revolution.extension.actions.min.js')}}"></script>
+	<script src="{{asset('frontend/plugins/rs-plugin/js/extensions/revolution.extension.kenburn.min.js')}}"></script>
+	<script src="{{asset('frontend/plugins/rs-plugin/js/extensions/revolution.extension.navigation.min.js')}}"></script>
+	<script src="{{asset('frontend/plugins/rs-plugin/js/extensions/revolution.extension.migration.min.js')}}"></script>
+	<script src="{{asset('frontend/plugins/rs-plugin/js/extensions/revolution.extension.parallax.min.js')}}"></script>
+	{{--<script src="{{asset('frontend/js/plugins.js')}}"></script>--}}
+	{{--<script src="{{asset('frontend/js/custom.js')}}"></script>--}}
+	{{--<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>--}}
+	{{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>--}}
+	{{--<script src="https://npmcdn.com/isotope-layout@3.0/dist/isotope.pkgd.min.js"></script>--}}
 	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJyWgPF1EBDeQjx4ctp4e_DuoLi7Zf8OA" type="text/javascript"></script>
-
-	<script>
-		(function() {
-			$.ajaxSetup({
-				headers: {
-					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-				}
-			});
-		})();
+	<script type="text/javascript">
+        var tpj = jQuery;
+        tpj.noConflict();
+        var revapi1;
+        tpj(document).ready(function() {
+            if (tpj("#rev_slider_1_1").revolution == undefined) {
+                revslider_showDoubleJqueryError("#rev_slider_1_1");
+            } else {
+                revapi1 = tpj("#rev_slider_1_1").show().revolution({
+                    sliderType: "standard",
+                    sliderLayout: "auto",
+                    dottedOverlay: "none",
+                    delay: 9000,
+                    navigation: {
+                        keyboardNavigation: "off",
+                        keyboard_direction: "horizontal",
+                        mouseScrollNavigation: "off",
+                        mouseScrollReverse: "default",
+                        onHoverStop: "on",
+                        touch: {
+                            touchenabled: "on",
+                            swipe_threshold: 75,
+                            swipe_min_touches: 1,
+                            swipe_direction: "horizontal",
+                            drag_block_vertical: false
+                        },
+                        arrows: {
+                            style: "hesperiden",
+                            enable: true,
+                            hide_onmobile: false,
+                            hide_onleave: false,
+                            tmp: '',
+                            left: {
+                                h_align: "left",
+                                v_align: "center",
+                                h_offset: 20,
+                                v_offset: 0
+                            },
+                            right: {
+                                h_align: "right",
+                                v_align: "center",
+                                h_offset: 20,
+                                v_offset: 0
+                            }
+                        }
+                    },
+                    visibilityLevels: [1240, 1024, 778, 480],
+                    gridwidth: 1100,
+                    gridheight: 720,
+                    lazyType: "none",
+                    shadow: 0,
+                    spinner: "spinner0",
+                    stopLoop: "off",
+                    stopAfterLoops: -1,
+                    stopAtSlide: -1,
+                    shuffle: "off",
+                    autoHeight: "off",
+                    hideThumbsOnMobile: "off",
+                    hideSliderAtLimit: 0,
+                    hideCaptionAtLimit: 0,
+                    hideAllCaptionAtLilmit: 0,
+                    debugMode: false,
+                    fallbacks: {
+                        simplifyAll: "off",
+                        nextSlideOnWindowFocus: "off",
+                        disableFocusListener: false,
+                    }
+                });
+            }
+        }); /*ready*/
 	</script>
+	<script>
+        jQuery(window).load(function() {
+            var retina = window.devicePixelRatio > 1 ? true : false;
+            if (retina) {
+                var retinaEl = jQuery("#logo img");
+                var retinaLogoW = retinaEl.width();
+                var retinaLogoH = retinaEl.height();
+                retinaEl.attr("src", "images/logo-retina.png").width(retinaLogoW).height(retinaLogoH)
+            }
+        });
+	</script>
+
+	{{--<script>--}}
+		{{--(function() {--}}
+			{{--$.ajaxSetup({--}}
+				{{--headers: {--}}
+					{{--'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')--}}
+				{{--}--}}
+			{{--});--}}
+		{{--})();--}}
+	{{--</script>--}}
     @yield('scripts')
 </body>
 </html>
