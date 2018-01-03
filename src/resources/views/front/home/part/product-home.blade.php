6ee82aeec99b6b657cd9f5b8329ce10e3d80edad
Tôi Là Tùng <toilatung@icloud.com>
2018-01-03 12:43:36 +0700

    Fix

--- src/resources/views/front/home/part/product-home.blade.php
+++ src/resources/views/front/home/part/product-home.blade.php
@@ -1,23 +1,30 @@
-<div id="products" class="section darkbg" style="padding-top:0px; padding-bottom:30px;">
+<div id="products" class="section woocommerce darkbg" style="padding-top:0px; padding-bottom:30px;">
     <div class="section_wrapper clearfix">
         <div class="items_group clearfix">
             <!-- One full width row-->
             <div class="column one column_fancy_heading">
                 <div class="fancy_heading">
                     <div class="items_group clearfix">
-                        @foreach($new_products as $product)
-                        <div class="column one-fourth column_trailer_box">
-                            <div class="trailer_box">
-                                <a href="{{url('/product')}}/{{$product->slug}}">
-                                    <img class="scale-with-grid" src="{{asset('/storage')}}/{{$product->GetMediaByOrderAsc()->thumb??'images/no-image.png'}}" alt="">
-                                    <div class="desc">
-                                        <h2 class="pro-title">{{$product->translation->name??$product->name}}</h2>
-                                        <div class="line"></div>
-                                    </div>
-                                </a>
+                         <div class="column one woocommerce-content">
+                            <div class="products_wrapper isotope_wrapper">
+                                <ul class="products grid col-4">
+                                    @foreach($new_products as $product)
+                                    <li class="product has-post-thumbnail">
+                                        <div class="item_wrapper">
+                                            <div class="hover_box hover_box_product">
+                                                <a href="{{url('/product')}}/{{$product->slug}}">
+                                                    <img class="scale-with-grid" src="{{asset('/storage')}}/{{$product->GetMediaByOrderAsc()->thumb??'images/no-image.png'}}" alt="">
+                                                </a>
+                                            </div>
+                                            <div class="desc">
+                                                <h4><a href="{{url('/product')}}/{{$product->slug}}">{{$product->translation->name??$product->name}}</a></h4>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    @endforeach
+                                </ul>
                             </div>
                         </div>
-                        @endforeach
                     </div>
                 </div>
             </div>
