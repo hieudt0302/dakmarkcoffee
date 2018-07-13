b173796afd3787df87fd59a8bfb7a3aadfcd7e0e
hieudt <hieudt0302@gmail.com>
2018-07-13 09:16:44 +0700

    Add slider function

--- src/resources/views/front/home/slider.blade.php
+++ src/resources/views/front/home/slider.blade.php
@@ -3,118 +3,26 @@
 	<!-- START REVOLUTION SLIDER 5.3.0.1 fullwidth mode -->
 	<div id="rev_slider_1_1" class="rev_slider fullwidthabanner tp-overflow-hidden" style="display:none;" data-version="5.3.0.1">
 		<ul>
+			@foreach($sliders as $slider)
 			<!-- SLIDE  -->
-			<li data-index="rs-17" data-transition="random" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="{{asset('frontend/images/slider/Tay-Nguyen_01-100x50.jpg')}}" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+			<li data-index="rs-1{{$slider->id}}" data-transition="random" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
 				<!-- MAIN IMAGE -->
-				<img src="{{asset('frontend/images/slider/Tay-Nguyen_01.jpg')}}" alt="" title="Tay-Nguyen" width="1800" height="1149" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
+				<img src="{{asset('/storage/'.$slider->image) }}" alt="" title="Tay-Nguyen" width="1800" height="1149" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
 				<!-- LAYERS -->
 				<!-- LAYER NR. 1 -->
-				<div class="tp-caption verylargetext-x   tp-resizeme" id="slide-17-layer-1" data-x="center" data-hoffset="1" data-y="450" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 5; white-space: nowrap; color: rgba(255, 255, 255, 1.00);text-transform:left;border-color:rgba(255, 255, 255, 1.00);">
+				<div class="tp-caption verylargetext-x   tp-resizeme" id="slide-{{$slider->id}}-layer-1"  data-x="center" data-hoffset="1" data-y="450" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 5; white-space: nowrap; color: rgba(255, 255, 255, 1.00);text-transform:left;border-color:rgba(255, 255, 255, 1.00);">
 					<table style="border-color: #ffffff;" border="2px" cellpadding="5px">
 						<tbody>
 						<tr>
 							<td>
-								<h2><span style="boder-radius: 2px; font-size: 32pt; font-weight: 300; color: #fff;">CÀ PHÊ TỪ NIỀM ĐAM MÊ VÀ TÂM HUYẾT</span></h2>
+								<h2><span style="boder-radius: 2px; font-size: 32pt; font-weight: 300; color: #fff;">{{$slider->translation->description}}</span></h2>
 							</td>
 						</tr>
 						</tbody>
 					</table>
-				</div>
-				<!-- LAYER NR. 2 -->
-				<div class="tp-caption black   tp-resizeme" id="slide-17-layer-2" data-x="319" data-y="343" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 6; white-space: nowrap;text-transform:left;border-width:0px;">
-					<h2 style="font-size: 66pt; font-weight: 700; color: #faa61a;">D<span style="font-size: 50pt;">AK</span>M<span style="font-size: 50pt;">ARK</span></h2> </div>
+				</div>			
 			</li>
-			<!-- SLIDE  -->
-			<li data-index="rs-15" data-transition="random" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="{{asset('frontend/images/slider/coffee3554_size1-100x50.jpg')}}" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
-				<!-- MAIN IMAGE -->
-				<img src="{{asset('frontend/images/slider/coffee3554_size1.jpg')}}" alt="" title="coffee3554_size1" width="1700" height="1133" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
-				<!-- LAYERS -->
-				<!-- LAYER NR. 3 -->
-				<div class="tp-caption largetext   tp-resizeme" id="slide-15-layer-3" data-x="center" data-hoffset="3" data-y="376" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"x:{-250,250};y:{-150,150};rX:{-90,90};rY:{-90,90};rZ:{-360,360};sX:0;sY:0;opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 7; white-space: nowrap; color: rgba(255, 255, 255, 1.00);text-transform:left;border-color:rgba(255, 255, 255, 1.00);">
-					<table style="border-color: #ffffff;" border="2px" cellpadding="5px">
-						<tbody>
-						<tr>
-							<td>
-								<h2><span style="boder-radius: 2px; font-size: 32pt; font-weight: 300; color: #fff;">CHỌN LỌC KĨ CÀNG TỪNG HẠT</span></h2>
-							</td>
-						</tr>
-						</tbody>
-					</table>
-				</div>
-				<!-- LAYER NR. 4 -->
-				<div class="tp-caption black   tp-resizeme" id="slide-15-layer-4" data-x="338" data-y="271" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"x:{-250,250};y:{-150,150};rX:{-90,90};rY:{-90,90};rZ:{-360,360};sX:0;sY:0;opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 8; white-space: nowrap;text-transform:left;border-width:0px;">
-					<h2 style="font-size: 66pt; font-weight: 700;color: #faa61a;">D<span style="font-size: 50pt;">AK</span>M<span style="font-size: 50pt;">ARK</span></h2> </div>
-			</li>
-			<!-- SLIDE  -->
-			<li data-index="rs-18" data-transition="random" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="{{asset('frontend/images/slider/thac-Dray-Sap-100x50.jpg')}}" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
-				<!-- MAIN IMAGE -->
-				<img src="{{asset('frontend/images/slider/thac-Dray-Sap.jpg')}}" alt="" title="thac-Dray-Sap" width="2000" height="1335" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
-				<!-- LAYERS -->
-				<!-- LAYER NR. 5 -->
-				<div class="tp-caption very_large_text   tp-resizeme" id="slide-18-layer-5" data-x="87" data-y="409" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 9; white-space: nowrap;text-transform:left;">
-					<table style="border-color: #ffffff;" border="2px" cellpadding="5px">
-						<tbody>
-						<tr>
-							<td>
-								<h2><span style="boder-radius: 2px; font-size: 32pt; font-weight: 300; color: #fff;">NGUỒN NƯỚC TRONG MÁT CHO HẠT CÀ PHÊ</span></h2>
-							</td>
-						</tr>
-						</tbody>
-					</table>
-				</div>
-				<!-- LAYER NR. 6 -->
-				<div class="tp-caption black   tp-resizeme" id="slide-18-layer-6" data-x="315" data-y="294" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 10; white-space: nowrap;text-transform:left;border-width:0px;">
-					<h2 style="font-size: 66pt; font-weight: 700; color: #faa61a;">D<span style="font-size: 50pt;">AK</span>M<span style="font-size: 50pt;">ARK</span></h2> </div>
-			</li>
-			<!-- SLIDE  -->
-			<li data-index="rs-16" data-transition="random" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="{{asset('frontend/images/slider/slide-8-100x50.jpg')}}" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
-				<!-- MAIN IMAGE -->
-				<img src="{{asset('frontend/images/slider/slide-8.jpg')}}" alt="" title="ob0024_beans_1320x2345mm.indd" width="1920" height="1091" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
-				<!-- LAYERS -->
-				<!-- LAYER NR. 7 -->
-				<div class="tp-caption verylargetext-x   tp-resizeme" id="slide-16-layer-7" data-x="center" data-hoffset="-23" data-y="center" data-voffset="68" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 11; white-space: nowrap; color: rgba(255, 255, 255, 1.00);text-transform:left;border-color:rgba(255, 255, 255, 1.00);">
-					<table style="border-color: #ffffff;" border="2px" cellpadding="5px">
-						<tbody>
-						<tr>
-							<td>
-								<h2><span style="boder-radius: 2px; font-size: 32pt; font-weight: 300; color: #fff;">CÀ PHÊ XUẤT KHẨU SIÊU SẠCH</span></h2>
-							</td>
-						</tr>
-						</tbody>
-					</table>
-				</div>
-				<!-- LAYER NR. 8 -->
-				<div class="tp-caption verylargetext-x   tp-resizeme" id="slide-16-layer-8" data-x="343" data-y="283" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 12; white-space: nowrap; color: rgba(255, 255, 255, 1.00);text-transform:left;border-color:rgba(255, 255, 255, 1.00);">
-					<h2 style="font-size: 66pt; font-weight: 700; color: #faa61a;">D<span style="font-size: 50pt;">AK</span>M<span style="font-size: 50pt;">ARK</span></h2> </div>
-			</li>
-			<!-- SLIDE  -->
-			<li data-index="rs-19" data-transition="random" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="{{asset('frontend/images/slider/roast_coffee-closeup-100x50.jpg')}}" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
-				<!-- MAIN IMAGE -->
-				<img src="{{asset('frontend/images/slider/roast_coffee-closeup.jpg')}}" alt="" title="roast_coffee-closeup" width="1600" height="1200" data-bgposition="center bottom" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
-				<!-- LAYERS -->
-				<!-- LAYER NR. 9 -->
-				<div class="tp-caption verylargetext-x   tp-resizeme" id="slide-19-layer-9" data-x="243" data-y="358" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 13; white-space: nowrap; color: rgba(255, 255, 255, 1.00);text-transform:left;border-color:rgba(255, 255, 255, 1.00);">
-					<table style="border-color: #ffffff;" border="2px" cellpadding="5px">
-						<tbody>
-						<tr>
-							<td>
-								<h2><span style="boder-radius: 2px; font-size: 32pt; font-weight: 300; color: #fff;">CÀ PHÊ RANG XAY HẢO HẠNG</span></h2>
-							</td>
-						</tr>
-						</tbody>
-					</table>
-				</div>
-				<!-- LAYER NR. 10 -->
-				<div class="tp-caption very_large_text   tp-resizeme" id="slide-19-layer-10" data-x="335" data-y="258" data-width="auto" data-height="auto" data-type="text" data-responsive_offset="on" data-frames='[{"from":"opacity:0;","speed":300,"to":"","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"to":"auto:auto;","ease":"nothing"}]' data-textAlign="['','','','']" style="z-index: 14; white-space: nowrap;text-transform:left;">
-					<h2 style="text-align: center;font-size: 66pt; font-weight: 700; color: #faa61a;">D<span style="font-size: 50pt;">AK</span>M<span style="font-size: 50pt;">ARK</span></h2></br>
-				</div>
-			</li>
-			<!-- SLIDE  -->
-			<li data-index="rs-35" data-transition="random" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-link="" data-target="_blank" data-thumb="{{asset('frontend/images/slider/youtube-100x50.jpg')}}" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
-				<!-- MAIN IMAGE -->
-				<img src="{{asset('frontend/images/slider/youtube.jpg')}}" alt="" title="youtube" width="2565" height="1437" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
-				<!-- LAYERS -->
-			</li>
+			@endforeach
 		</ul>
 		<div class="tp-bannertimer" style="height: 5px; background-color: rgba(0, 0, 0, 0.15);"></div>
 	</div>
